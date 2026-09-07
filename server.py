4dda1fb9b783c052e56538423ae766b84b3f695d
Corevirus-ops <corvirus1738@gmail.com>
2026-09-07 01:12:20 -0400

    able to roll up the server to deliver the proper response showing the emotional breakdown using the detector

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    text_to_analyze = request.args.get("textToAnalyze")
+    text = emotion_detector(text_to_analyze)
+    text = f"For the given statement, the system response is {text}."
+    return text
+
+app.run(host = '0.0.0.0', port = 5000)
